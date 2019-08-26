39149e7d0564fe46c34455554b3de6d792fe7e7f
gm257496 <guillaume;mestrallet@cea.fr>
2019-08-26 12:56:43 +0200

    Modification du HTML du site

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 26 12:16:05 2019
+
+@author: gm257496
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
